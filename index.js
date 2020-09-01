5171335b79ba7e5f27b65c585b6c2c0e0b962cf1
DmitriyIvanovIT <strela199720@gmail.com>
2020-09-01 16:06:02 +0300

    feat: logic task 1 | script/script.js

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 'use strict';
 
-let allNumbers = [1, 2, 4, 5, 6, 7, 8],
-    someNumbers = [1, 2, 'Hello', 4, 5, 'world', 6, 7, 8],
-    noNumbers = ['здесь', 'нет', 'чисел'];
+const cartPrice = (price, count, promo) => {
+    if (count >= 5 && count < 10) {
+        price *= 0.95;
+    } else if (count >= 10) {
+        price *= 0.9;
+    }
 
-function isNumber(val) {
-    return typeof val === 'number';
-}
+    if (price > 100000 && price < 150000) {
+        price *= 0.8;
+    } else if (price >= 150000) {
+        price -= 50000;
+    }
 
-const isSomeTrue = function(array, func) {
-    let i = 0;
-
-    const filterFunc = (item) => {
-        if (i < (array.length - 1)) {
-            if (!func(item)) {
-                i++;
-                filterFunc(array[i]);
+    if (promo !== undefined) {
+        if (promo === '15' && price >= 25000) {
+            price *= 0.85;
+        } else if (promo === '100') {
+            if (price <= 100) {
+                price = 0;
             } else {
-                return;
+                price -= 100;
             }
         }
-    };
+    }
 
-    filterFunc(array[i]);
-
-    return func(array[i]);
-    
+    return price;
 };
 
-console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
-console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
-console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
+console.log(cartPrice(150000, 5, '100'));
